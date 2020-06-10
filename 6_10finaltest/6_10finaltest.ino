b1b5c849efecb829c7bbe84282531eaed3ae063d
HoldTing <57693128+HoldTing@users.noreply.github.com>
2020-06-10 10:42:40 +0800

    Add files via upload

---
+++ 6_10finaltest/6_10finaltest.ino
@@ -0,0 +1,44 @@
+#include <Ultrasonic.h>
+#include <LWiFi.h>
+#include <WiFiClient.h>
+#include "MCS.h"
+#define _SSID "AA202_2G"
+#define _KEY  "iteaa202"
+
+Ultrasonic ultrasonic(12, 13);
+int distance;
+MCSDevice mcs("Ddkp21B6", "xz3X2o8iCOU8Dikf");
+MCSDisplayInteger Distance("Distance");
+void setup() {
+  // put your setup code here, to run once:
+Serial.begin(9600);
+  Serial.println(F("Ultrasonicxx test!"));
+
+  while(WL_CONNECTED != WiFi.status())
+  {
+    Serial.print("WiFi.begin(");
+    Serial.print(_SSID);
+    Serial.print(",");
+    Serial.print(_KEY);
+    Serial.println(")...");
+    WiFi.begin(_SSID, _KEY);
+  }
+  Serial.println("WiFi connected !!");
+
+  mcs.addChannel(Distance);
+
+  while(!mcs.connected())
+  {
+    Serial.println("MCS.connect()...");
+    mcs.connect();
+  }
+  Serial.println("MCS connected !!");
+}
+
+void loop() {
+  distance = ultrasonic.read();
+  
+  Serial.print("Distance in CM: ");
+  Serial.println(distance);
+  delay(1000);
+}
